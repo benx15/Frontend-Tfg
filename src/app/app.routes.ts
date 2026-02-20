68f6e66e32711660c59f08f2559b7cd817b87cb3
Beñat <zabalaortizbezo@gmail.com>
2026-02-20 23:43:43 +0100

    por grupo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-
+import { SinGrupoComponent } from './pages/sin-grupo/sin-grupo.component';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'registro', component: RegisterComponent }
+  { path: 'registro', component: RegisterComponent },
+  { path: 'sin-grupo', component: SinGrupoComponent },
+  { path: 'user', component: UserComponent },
 ];
